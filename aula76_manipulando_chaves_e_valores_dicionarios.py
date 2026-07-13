9f9351fdecffb4dd4a6a73e24302f6ef7cdb543e
William <convidadoc38@gmai.com>
2026-07-13 09:37:15 -0300

    aprendendido sobre dicionario e como manipula-los e também função dentro de função e como retorna-las

---
+++ aula76_manipulando_chaves_e_valores_dicionarios.py
@@ -0,0 +1,30 @@
+# Manipulando chaves e valores em dicionários
+pessoa = {}
+
+##
+##
+
+chave = 'nome'
+
+pessoa[chave] = 'Luiz Otávio'
+pessoa['sobrenome'] = 'Miranda'
+
+
+print(pessoa[chave])
+
+pessoa[chave] = 'Maria'
+
+del pessoa['sobrenome']
+print(pessoa)
+print(pessoa['nome'])
+
+# pessoa.get('sobrenome')) este método é usado para saber se uma chave existe ou não
+# se a chave não existe, então por padrão seu retorno é None
+# agora se ela existe, então pode-se acessar seu valor
+
+if pessoa.get('sobrenome') is None:
+    print('NÃO EXISTE')
+else:
+    print(pessoa['sobrenome'])
+
+# print('ISSO Não vai')
